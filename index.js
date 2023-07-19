f14500880292221fd058ccec692eb14c28f06d5c
Aastha <saxena.aastha91@gmail.com>
2023-07-19 15:39:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+function moveDodgerLeft(){
+    dodger.style.left = "170px"
+
+}
+
+function moveDodgerRight(){
+    dodger.style.left = "178px"
+}
